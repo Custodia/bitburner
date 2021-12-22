e8e7472769dcb13de6438d82decb0ba36739d2d0
Jani Mäkelä <jmakela@synack.com>
2021-12-22 02:11:44 +0200

    Fix single iteration hack/grow/weaken scripts

--- scripts/grow.js
+++ scripts/grow.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns **/
 export async function main(ns) {
   const iterations = ns.args[1] || Number.MAX_SAFE_INTEGER
-  for (let i = 1; i < iterations; i++) {
+  for (let i = 1; i <= iterations; i++) {
     const target = ns.args[0] || primeTarget || 'joesguns'
     await ns.grow(target)
   }
