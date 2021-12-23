2f298904de4c1fad414b1954b760787b83e20d3a
Jani Mäkelä <jmakela@synack.com>
2021-12-23 15:45:38 +0200

    Always assign leftover threads to run basic_hack
    Simplifies the logic and should allow us to always just call
    bootstrap_advanced.js instead of bootstrap.js

--- scripts/bootstrap_advanced.js
+++ scripts/bootstrap_advanced.js
@@ -30,7 +30,8 @@ export async function main(ns) {
   let availableThreads = maxAvailableThreads
   let pickedHosts = []
 
-  while (availableThreads > 10) {
+  // Assign threads that fit
+  while (availableThreads > 0) {
     if (hackableHosts.length === 0) {
       ns.toast(`No hackable hosts left, running on ${currentHost}, adding used hosts`)
       hackableHosts = Object.values(hostData)
@@ -41,24 +42,30 @@ export async function main(ns) {
     const hostFits = hostsThatFit.length > 0
     const bestHost = hostFits ? hostsThatFit[0] : hackableHosts[0]
 
-    const { weakenPortion, growPortion, hackPortion } = bestHost.hackStats
-    const weakenThreads = hostFits ? bestHost.hackStats.weakenThreads : Math.floor(weakenPortion * availableThreads)
-    const growThreads = hostFits ? bestHost.hackStats.growThreads : Math.floor(growPortion * availableThreads)
-    const hackThreads = hostFits ? bestHost.hackStats.hackThreads : Math.floor(hackPortion * availableThreads)
-
     if (hostFits) {
+      const { weakenPortion, growPortion, hackPortion } = bestHost.hackStats
+      const weakenThreads = hostFits ? bestHost.hackStats.weakenThreads : Math.floor(weakenPortion * availableThreads)
+      const growThreads = hostFits ? bestHost.hackStats.growThreads : Math.floor(growPortion * availableThreads)
+      const hackThreads = hostFits ? bestHost.hackStats.hackThreads : Math.floor(hackPortion * availableThreads)
+
       hackableHosts = hackableHosts.filter(host => host.hostname !== bestHost.hostname)
       pickedHosts.push(bestHost.hostname)
       nodeClaims[currentHost] = pickedHosts
-    }
-    availableThreads -= weakenThreads + growThreads + hackThreads
 
-    if (weakenThreads > 0 && growThreads > 0 && hackThreads > 0) {
+      availableThreads -= weakenThreads + growThreads + hackThreads
+
       ns.run('weaken.js', weakenThreads, bestHost.hostname)
       ns.run('grow.js', growThreads, bestHost.hostname)
       ns.run('hack.js', hackThreads, bestHost.hostname)
     } else {
-      ns.print(`Not enough available threads to hack ${bestHost.hostname}, there were ${availableThreads} left over.`)
+      primeTarget = bestHost.hostname
+      availableThreads = 0
     }
   }
+
+  // Assign leftover threads to target primeTarget
+  const [totalRam, usedRam] = ns.getServerRam(currentHost)
+  await ns.scp('basic_hack.js', 'home', currentHost)
+  const leftoverThreads = (totalRam - usedRam + ns.getScriptRam('bootstrap_advanced.js')) / ns.getScriptRam('basic_hack.js')
+  ns.spawn('basic_hack.js', leftoverThreads)
 }
