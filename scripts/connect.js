68f45a5167ae62bbd71e5a8e166a92818bf69e16
Jani Mäkelä <jmakela@synack.com>
2021-12-23 14:14:42 +0200

    Create lib function for running console commands

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,5 +1,5 @@
 /** @param {NS} ns **/
-import { findConnectionPath } from 'lib.js'
+import { findConnectionPath, runConsoleCommand } from 'lib.js'
 
 export function autocomplete(data, _args) {
   return [...data.servers]
@@ -13,19 +13,5 @@ export async function main(ns) {
   const path = findConnectionPath(ns, ns.args[0])
   path.shift()
   const command = path.map(server => `connect ${server}`).join("; ")
-
-  // Acquire a reference to the terminal text field
-  const terminalInput = document.getElementById("terminal-input");
-
-  // Set the value to the command you want to run.
-  terminalInput.value=command;
-
-  // Get a reference to the React event handler.
-  const handler = Object.keys(terminalInput)[1];
-
-  // Perform an onChange event to set some internal values.
-  terminalInput[handler].onChange({target:terminalInput});
-
-  // Simulate an enter press
-  terminalInput[handler].onKeyDown({keyCode:13,preventDefault:()=>null});
+  runConsoleCommand(command)
 }
--- scripts/lib.js
+++ scripts/lib.js
@@ -1,4 +1,21 @@
 /** @param {NS} ns **/
+export function runConsoleCommand(command) {
+  // Acquire a reference to the terminal text field
+  const terminalInput = document.getElementById("terminal-input");
+
+  // Set the value to the command you want to run.
+  terminalInput.value=command;
+
+  // Get a reference to the React event handler.
+  const handler = Object.keys(terminalInput)[1];
+
+  // Perform an onChange event to set some internal values.
+  terminalInput[handler].onChange({target:terminalInput});
+
+  // Simulate an enter press
+  terminalInput[handler].onKeyDown({keyCode:13,preventDefault:()=>null});
+}
+
 export function findConnectionPath(ns, toFind, toScan = 'home', path = []) {
   const newPath = path.concat(toScan)
   const currentScan = ns.scan(toScan)
